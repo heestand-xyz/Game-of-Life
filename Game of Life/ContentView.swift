2cef05850a7b6806e09da996c3baa193de8e2f10
Anton Heestand <anton.heestand@gmail.com>
2022-01-11 21:51:34 +0100

    Initial Commit

---
+++ Game of Life/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Game of Life
+//
+//  Created by Anton Heestand on 2022-01-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Game of Life/Game_of_LifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_of_LifeApp.swift
+//  Game of Life
+//
+//  Created by Anton Heestand on 2022-01-11.
+//
+
+import SwiftUI
+
+@main
+struct Game_of_LifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
