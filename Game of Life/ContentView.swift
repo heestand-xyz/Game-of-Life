3f72bf6e56a9f025a67fa0cbe64552b0f820e024
Anton Heestand <anton.heestand@gmail.com>
2022-01-11 22:37:29 +0100

    Feedback Fix

--- Game of Life/ContentView.swift
+++ Game of Life/ContentView.swift
@@ -6,11 +6,41 @@
 //
 
 import SwiftUI
+import RenderKit
+import PixelKit
 
 struct ContentView: View {
+    
+    @StateObject var polygonPix = PolygonPIX(at: .square(1000))
+    @StateObject var feedbackPix = FeedbackPIX()
+    @StateObject var metalEffectPix = MetalEffectPIX()
+    @StateObject var crossPix = CrossPIX()
+    var finalPix: PIX { metalEffectPix }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        PixelView(pix: finalPix)
+            .onAppear {
+                polygonPix.radius = 0.05
+                
+                feedbackPix.input = polygonPix
+                feedbackPix.viewInterpolation = .nearest
+                
+                metalEffectPix.input = feedbackPix
+                metalEffectPix.code = """
+                //float2 pos = float2(v, u);
+                return 1;//tex.sample(s, pos);
+                """
+                
+                crossPix.fraction = 0.5
+                crossPix.inputA = polygonPix
+                crossPix.inputB = metalEffectPix
+                
+                feedbackPix.feedbackInput = crossPix
+                
+//                polygonPix.render()
+//                metalEffectPix.render()
+//                crossPix.render()
+            }
     }
 }
 
