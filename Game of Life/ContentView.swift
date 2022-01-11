06349cb1037ddc47a132bac89e54d6166f8bd978
Anton Heestand <anton.heestand@gmail.com>
2022-01-11 23:29:40 +0100

    The Game

--- Game of Life/ContentView.swift
+++ Game of Life/ContentView.swift
@@ -9,38 +9,92 @@ import SwiftUI
 import RenderKit
 import PixelKit
 
+class GameOfLife: ObservableObject {
+    
+    let polygonPix = PolygonPIX(at: .square(2000))
+//    let starPix = StarPIX(at: .square(1000))
+//    let noisePix = NoisePIX(at: .square(1000))
+    var sourcePix: (PIX & NODEOut)!
+    let feedbackPix = FeedbackPIX()
+    let metalEffectPix = MetalEffectPIX()
+    let transformPix = TransformPIX()
+    
+    let blendPix = BlendPIX()
+    let finalPix: PIX!
+    
+    
+    init() {
+        polygonPix.radius = 1.0 / 3.0
+//        starPix.leadingRadius = 0.25
+//        starPix.trailingRadius = 0.125
+//        starPix.cornerRadius = 0.01
+//        noisePix.octaves = 10
+//        noisePix.zoom = 0.1
+        
+        sourcePix = polygonPix
+        
+        feedbackPix.input = sourcePix
+        feedbackPix.viewInterpolation = .nearest
+        
+        metalEffectPix.input = feedbackPix
+        metalEffectPix.code = """
+        bool center = tex.sample(s, uv).r > 0.5;
+        bool topLeft = tex.sample(s, float2(u - wu, v - hv)).r > 0.5;
+        bool top = tex.sample(s, float2(u, v - hv)).r > 0.5;
+        bool topRight = tex.sample(s, float2(u + wu, v - hv)).r > 0.5;
+        bool right = tex.sample(s, float2(u + wu, v)).r > 0.5;
+        bool bottomRight = tex.sample(s, float2(u + wu, v + hv)).r > 0.5;
+        bool bottom = tex.sample(s, float2(u, v + hv)).r > 0.5;
+        bool bottomLeft = tex.sample(s, float2(u - wu, v + hv)).r > 0.5;
+        bool left = tex.sample(s, float2(u - wu, v)).r > 0.5;
+        int neighbours = 0;
+        if (topLeft) { neighbours += 1; }
+        if (top) { neighbours += 1; }
+        if (topRight) { neighbours += 1; }
+        if (right) { neighbours += 1; }
+        if (bottomRight) { neighbours += 1; }
+        if (bottom) { neighbours += 1; }
+        if (bottomLeft) { neighbours += 1; }
+        if (left) { neighbours += 1; }
+        bool alive = center;
+        if (alive) {
+            if (neighbours < 2) {
+                alive = false;
+            } else if (neighbours > 3) {
+                alive = false;
+            }
+        } else {
+            if (neighbours == 3) {
+                alive = true;
+            }
+        }
+        return float4(float3(alive), 1.0);
+        """
+        
+        transformPix.input = metalEffectPix
+        transformPix.scale = 1.01
+        
+        blendPix.blendMode = .difference
+        blendPix.inputA = transformPix
+        blendPix.inputB = sourcePix
+        
+        feedbackPix.feedbackInput = blendPix
+        
+        finalPix = metalEffectPix// + sourcePix * 0.1
+        
+        Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { _ in
+            self.polygonPix.rotation += 0.002
+        }
+    }
+    
+}
+
 struct ContentView: View {
     
-    @StateObject var polygonPix = PolygonPIX(at: .square(1000))
-    @StateObject var feedbackPix = FeedbackPIX()
-    @StateObject var metalEffectPix = MetalEffectPIX()
-    @StateObject var crossPix = CrossPIX()
-    var finalPix: PIX { metalEffectPix }
+    @StateObject var gameOfLife = GameOfLife()
     
     var body: some View {
-        PixelView(pix: finalPix)
-            .onAppear {
-                polygonPix.radius = 0.05
-                
-                feedbackPix.input = polygonPix
-                feedbackPix.viewInterpolation = .nearest
-                
-                metalEffectPix.input = feedbackPix
-                metalEffectPix.code = """
-                //float2 pos = float2(v, u);
-                return 1;//tex.sample(s, pos);
-                """
-                
-                crossPix.fraction = 0.5
-                crossPix.inputA = polygonPix
-                crossPix.inputB = metalEffectPix
-                
-                feedbackPix.feedbackInput = crossPix
-                
-//                polygonPix.render()
-//                metalEffectPix.render()
-//                crossPix.render()
-            }
+        PixelView(pix: gameOfLife.finalPix)
     }
 }
 
